d964bea9ac5e2a541f165180315c6a2feadb7295
nickzmra <nzamora9@stu.southtexascollege.edu>
2021-03-28 22:05:29 -0500

    First Commit - Gif App

---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="py-6 bg-gray-800 flex justify-center">
+    <router-link
+      to="/"
+      class="bg-clip-text font-bold tracking-widest text-4xl text-transparent bg-gradient-to-r from-green-400 to-blue-500"
+      >Search GIFS (:</router-link
+    >
+  </div>
+</template>
